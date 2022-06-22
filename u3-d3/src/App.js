d06eb31a2c6b2eb50e6ea5915450a217a1605168
marios <mgeorgak859@gmail.com>
2022-06-22 15:25:01 +0300

    added alert component

--- u3-d3/src/App.js
+++ u3-d3/src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import WarningSign from './components/WarningSign';
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <WarningSign msg="hello"/>
+    </div>
   );
 }
 
