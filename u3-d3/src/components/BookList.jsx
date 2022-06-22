771af594e5db6b9a631eb64d0db6f6a3bde0fc38
marios <mgeorgak859@gmail.com>
2022-06-22 18:11:27 +0300

    added filterBookList component

--- u3-d3/src/components/BookList.jsx
+++ u3-d3/src/components/BookList.jsx
@@ -1,12 +1,32 @@
+import React from "react";
 import SingleBook from "./SingleBook";
+import { Form } from "react-bootstrap"
 
-const BookList = ({books}) => {
-    return(
-        <div>
-            {books.map((current)=>{
-                return <SingleBook book={current}/>
-            })}
-        </div>
-    )
+class BookList extends React.Component {
+
+    state = {
+        searchQuery: ''
+    }
+
+    render() {
+        return (
+            <div>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Search</Form.Label>
+                    <Form.Control 
+                    type="text" 
+                    placeholder="Search here" 
+                    value={this.state.searchQuery}
+                    onChange={e=> this.setState({searchQuery: e.target.value})}
+                    
+                    />
+                   
+                </Form.Group>
+                {this.props.books.map((current) => {
+                    return <SingleBook book={current} />
+                })}
+            </div>
+        )
+    }
 }
 export default BookList
---
+++ u3-d3/src/components/filterBookList.jsx
@@ -0,0 +1,8 @@
+import BookList from "./BookList"
+
+
+const filterBookList = () => {
+    return (
+        <div></div>
+    )
+}
