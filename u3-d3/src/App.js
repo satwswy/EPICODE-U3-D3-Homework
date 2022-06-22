24a0dcac53fcabe1467ec8b6308a2fcea9365ff7
marios <mgeorgak859@gmail.com>
2022-06-22 15:42:18 +0300

    added MyBadge component

--- u3-d3/src/App.js
+++ u3-d3/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import WarningSign from './components/WarningSign';
+import MyBadge from './components/MyBadge';
 
 function App() {
   return (
     <div>
       <WarningSign msg="hello"/>
+      <MyBadge text="hello world" textColor="blue" />
     </div>
   );
 }
